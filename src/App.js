d60ab743d51e3980fb153280792b029811cb666d
rafi ullah <rafiullah372@gmail.com>
2021-12-09 02:30:02 +0500

    Deleted extra files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,4 @@
-// import {useState}  from 'react'
-// import database from './firebare';
 
-// function App() {
-//   const [name, setName] = useState();
-//   const [age, setAge] = useState();
-
-//   const Push = () =>{
-//     database.ref('user').set({
-//       name:name,
-//       age:age
-//     })
-//   }
-//   return (
-//     <div className="App">
-//      <div className="row ">
-       
-//        <div className="col-md-6">
-//          <div className="form-group">
-
-//         <input type="text" className="form-control" placeholder="name" 
-//         value={name}
-//         onChange={(e)=>setName(e.target.value)}/>
-//          </div>
-//          <div className="form-group">
-//         <input type="text" className="form-control" placeholder="age" 
-//         value={age}
-//         onChange={(e)=>setAge(e.target.value)}/>
-//          </div>
-//          {/* <input type="submit" onClick={Push}/> */}
-//         <button onClick={Push}>Add</button>
-//        </div>
-      
-//      </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from 'react'
 import Todo from './Todo'
 import './App.css'
